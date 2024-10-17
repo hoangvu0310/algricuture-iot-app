971d2f67007b09bc737f6929c4035a904df6f62b
hoangvu0310 <hoangvu031003@gmail.com>
2024-10-18 01:05:51 +0700

    init project

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	semi: false,
+	trailingComma: 'all',
+	singleQuote: true,
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+	bracketSpacing: true,
+	arrowParens: 'always',
+	endOfLine: 'lf',
+}
